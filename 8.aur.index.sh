d7d1453c3ac2483db737839b4c67f793d8f5116d
gnuhub <gnuhub@gmail.com>
2022-11-11 18:30:54 +0800

    auto commit

--- 8.aur.index.sh
+++ 8.aur.index.sh
@@ -11,3 +11,12 @@ cd $CMD_PATH
 
 sed -i "/\* main/d" 7.aur.all.txt
 sed -i "s/  remotes\/origin\///g" 7.aur.all.txt
+
+mkdir -p aur-all
+while read line
+do
+    echo $line
+    index=${line:0:1}
+    mkdir -p aur-all/$index 
+    touch aur-all/$index/$line
+done < 7.aur.all.txt
