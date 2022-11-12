9a5c31899beabe6f97326aa99a4ab936c1a02828
gnuhub <gnuhub@gmail.com>
2022-11-12 16:57:09 +0800

    auto commit

--- 11.branches.sh
+++ 11.branches.sh
@@ -9,6 +9,9 @@ export PROJECT_NAME="${CMD_PATH##*/}"
 
 cd $CMD_PATH
 
+git remote add archlinuxaur366 git@github.com:archlinuxaur366/9329-archlinux-aur-build.git
+git remote add archlinuxaur367 git@github.com:archlinuxaur367/9329-archlinux-aur-build.git
+git remote add archlinuxaur368 git@github.com:archlinuxaur368/9329-archlinux-aur-build.git
 cd aur-all 
 export n=1
 git checkout dev
@@ -20,6 +23,10 @@ echo $n
     git checkout -b $f 
     git checkout $f 
     git merge dev 
+
     p2
+    git push archlinuxaur366 $f
+    git push archlinuxaur367 $f
+    git push archlinuxaur368 $f
     cd aur-all
 done
